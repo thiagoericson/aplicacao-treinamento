1e21922404031408d33339f319c075b75cc607ac
Thiago Cabral <ericson.thiago@gmail.com>
2021-03-28 14:22:16 -0300

    Operadores logicos, AND e OR

---
+++ br/com/treinamento/Operadores.java
@@ -0,0 +1,13 @@
+package br.com.treinamento;
+
+public class Operadores {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        if (num1 == 20 || num2 == 20) {
+            System.out.println("TRUE");
+        }else{
+            System.out.println("FALSE");
+        }
+    }
+}
